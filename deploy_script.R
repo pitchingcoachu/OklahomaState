fc6c9d662bc1e711993766cf755bbd49819ca6e4
pitchingcoachu <jgaynor@pitchingcoachu.com>
2025-12-27 23:12:42 -0700

    Change app name from 'gcubaseball' to 'osubaseball'

--- deploy_script.R
+++ deploy_script.R
@@ -59,7 +59,7 @@ deploy_app <- function() {
     cat("Deploying to shinyapps.io...\n")
     deployApp(
       appDir = ".",
-      appName = "gcubaseball",
+      appName = "osubaseball",
       forceUpdate = TRUE,
       launch.browser = FALSE,
       logLevel = "verbose"
